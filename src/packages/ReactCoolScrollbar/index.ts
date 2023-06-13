619117e61f1bf539c65b8488198f77d5add6d921
ekhlasmridha <ekhlasmridha@gmail.com>
2023-06-14 03:38:51 +0600

    merge conflict resolved

--- src/packages/ReactCoolScrollbar/index.ts
+++
@@ -1,3 +0,0 @@
-export { ReactCoolScrollbar } from "./CoolScrollbar";
-export type * from "./scroller.types";
-import "./style.scss";
---
+++ src/packages/index.ts
@@ -0,0 +1,3 @@
+export { ReactCoolScrollbar } from "./ReactCoolScrollbar";
+export type {CoolScrollbarProps,ScrollbarThumbProps,ScrollbarTrackProps} from "./scroller.types";
+import "./style.scss";
